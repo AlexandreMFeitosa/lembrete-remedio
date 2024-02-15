a86b8a33c14b8518ecfd08de668b4619ed622e1a
AlexandreMFeitosa <alexandremoraesfeitosa@gmail.com>
2024-02-14 21:41:22 -0300

    test fail

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+
+let nameRemedio = document.getElementById('nameRemedio');
+let totalRemedio = document.getElementById('totalRemedio');
+let avisoDeFim = document.getElementById('avisoFim')
+
+function verificarQuantidade() {
+
+    if (totalRemedio < 10) {
+        alert(`teste 1`)
+
+    } else {
+        
+       let avisoDeFim = `teste ok`
+    }
+}
