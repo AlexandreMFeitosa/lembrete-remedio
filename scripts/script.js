240828ec972b205afa94dda91ba009a61a872a72
Alexandre Moraes <133717322+AlexandreMFeitosa@users.noreply.github.com>
2024-03-03 11:59:55 -0300

    Add verificador de campo em branco (#5)
    ok

--- scripts/script.js
+++ scripts/script.js
@@ -6,18 +6,21 @@ let nameRemedio = document.getElementById('nameRemedio').value;
 let totaldeRemedio = parseInt(document.getElementById('totalRemedio').value);
 let avisoDeFim = document.getElementById('avisoFim');
 
-
-if (totaldeRemedio <= 5) {
-    alert("Atenção ! Estoque baixo !");
-    avisoDeFim.innerHTML =`Estoque de ${nameRemedio} está baixo ! 
-    Total de comprimidos = <mark>${totaldeRemedio}</mark>`;
-
+if(nameRemedio.length === 0 || isNaN(totaldeRemedio) ) {
+    alert("Favor preencher os campos obrigatórios !")
 } else {
-    alert("Estoque normal !")
-   avisoDeFim.innerHTML = `<strong>Estoque de ${nameRemedio} está normal ! 
-   Total de comprimidos = <mark>${totaldeRemedio}</mark>`;
-
+    if (totaldeRemedio <= 5) {
+        alert("Atenção ! Estoque baixo !");
+        avisoDeFim.innerHTML =`Estoque de ${nameRemedio} está baixo ! 
+        Total de comprimidos = <mark>${totaldeRemedio}</mark>`;
+    
+    } else {
+        alert("Estoque normal !")
+       avisoDeFim.innerHTML = `<strong>Estoque de ${nameRemedio} está normal ! 
+       Total de comprimidos = <mark>${totaldeRemedio}</mark>`;
+    }
 }
+
 }
 
 // Agora vou adicionar um botao, que ao clicar nele fará um reset, e todos os inputs anteriormente preenchidos ficaram em branco !
